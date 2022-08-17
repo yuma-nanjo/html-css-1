35a5f51bd88e6373f4b6ea18e5195feafe56cefe
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-17 12:42:49 +0900

    commit

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/bb.png" type="image/png" />
+          <link rel="icon" href="/bb.svg" type="image/svg+xml" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
